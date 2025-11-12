85e3361941219cafef1a57ce9dbd5097b3d3adb8
Youssef Ghoneim <YoussefGhoneim2311@gmail.com>
2025-11-12 20:11:06 +0200

    trying

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../style/Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <div className="w-100">
-        <nav className="navbar-personal">
-            <div className="logo-section-personal">
-                <img src="/logo.svg" alt="تحيا مصر" className="logo-personal" />
-                <h1 className="site-title-personal">خدمتي الإلكترونية</h1>
-            </div>
+      <nav className="navbar-personal">
+        <div className="logo-section-personal">
+          <img src="/logo.svg" alt="تحيا مصر" className="logo-personal" />
+          <h1 className="site-title-personal">خدمتي الإلكترونية</h1>
+        </div>
 
-            <ul className="nav-links-personal">
-                <li>
-                <NavLink to="/" end>
-                    الصفحة الرئيسية
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/services">الخدمات الإلكترونية</NavLink>
-                </li>
-                <li>
-                <NavLink to="/public">الخدمات العامة</NavLink>
-                </li>
-                <li>
-                <NavLink to="/about">عن الموقع</NavLink>
-                </li>
-                <li>
-                <NavLink to="/contact">تواصل معنا</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <div className="menu-icon-personal" onClick={toggleMenu}>
+          <i className={menuOpen ? "bx bx-x" : "bx bx-menu"}></i>
+        </div>
+
+        <ul className={`nav-links-personal ${menuOpen ? "open" : ""}`}>
+          <li>
+            <NavLink to="/" end onClick={closeMenu}>
+              الصفحة الرئيسية
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" onClick={closeMenu}>
+              الخدمات الإلكترونية
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/public" onClick={closeMenu}>
+              الخدمات العامة
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" onClick={closeMenu}>
+              عن الموقع
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={closeMenu}>
+              تواصل معنا
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
-
 export default Navbar;
-
